88551180f2719f401411b83775af1d3e98511e42
Zikun Lyu <zikun.lyu@mail.mcgill.ca>
2019-08-30 16:11:08 +0800

    successfully add filter into gallery in family page

--- js/family.js
+++ js/family.js
@@ -1,9 +1,30 @@
 
 $(document).ready (function () {
-    $(".fancybox").fancybox();
-    filterSelection("all");
+    // external js: isotope.pkgd.js
 
-
+// init Isotope
+var $grid2 = $('.row').isotope({
+    itemSelector: '.portfolio-post',
+    layoutMode: 'fitRows'
+  });
+  
+  // bind filter button click
+  $('.filters-button-group').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    //filterValue = filterFns[ filterValue ] || filterValue;
+    filterValue =  filterValue;
+    $grid2.isotope({ filter: filterValue });
+  });
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+  
 });		
 
 
