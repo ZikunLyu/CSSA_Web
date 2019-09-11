531865058079f677185b02b80504425d5b5c00c5
Zikun Lyu <zikun.lyu@mail.mcgill.ca>
2019-09-11 16:51:47 +0800

    update the preloading page

--- js/plugin.js
+++ js/plugin.js
@@ -1,31 +1,36 @@
-$(window).on("load", function(){
-  $(".loader-wrapper").fadeOut("slow");
-  $("body").removeClass("preload");
-});
+
 
 $(document).ready(function () {
-      window.sr = ScrollReveal();
-      sr.reveal('.navbar', {
-        duration: 2000,
-        origin: 'bottom'
-       });
+  $(window).on("load", function () {
+    //$("body").hide();
+    $(".loader-wrapper").fadeOut("slow");
+    //$("body").removeClass("preload");
+    //$("body").show();
+  });
 
-     $('#search').click(function() {
-       $('.nav-link').addClass('hide-item')
-       $('.search-form').addClass('active')
-       $('.close').addClass('active')
-       $('#login').addClass('deactive')
-       $('#search').hide()
-       $('#login').hide()
-     })
-     $('.close').click(function() {
-       $('.nav-link').removeClass('hide-item')
-       $('.search-form').removeClass('active')
-       $('.close').removeClass('active')
-       $('#login').removeClass('deactive')
-       $('#search').show()
-       $('#login').show()
-     })
-    
-   })
+
+  window.sr = ScrollReveal();
+  sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom'
+  });
+
+  $('#search').click(function () {
+    $('.nav-link').addClass('hide-item')
+    $('.search-form').addClass('active')
+    $('.close').addClass('active')
+    $('#login').addClass('deactive')
+    $('#search').hide()
+    $('#login').hide()
+  })
+  $('.close').click(function () {
+    $('.nav-link').removeClass('hide-item')
+    $('.search-form').removeClass('active')
+    $('.close').removeClass('active')
+    $('#login').removeClass('deactive')
+    $('#search').show()
+    $('#login').show()
+  })
+
+})
 
