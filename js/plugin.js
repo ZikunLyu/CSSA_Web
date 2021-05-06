109679602f10096ad9a977b8091a3e1f9ef2ca3a
ZeroLifeCat <mandylogan8x@gmail.com>
2021-05-06 11:40:30 -0400

    Test

---
+++ js/plugin.js
@@ -0,0 +1,36 @@
+
+
+$(document).ready(function () {
+  $(window).on("load", function () {
+    $(".loader-wrapper").fadeOut("slow"); 
+    /*
+    $(".se-pre-con").fadeOut("slow");;
+    */
+  });
+
+
+  window.sr = ScrollReveal();
+  sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom'
+  });
+
+  $('#search').click(function () {
+    $('.nav-link').addClass('hide-item')
+    $('.search-form').addClass('active')
+    $('.close').addClass('active')
+    $('#login').addClass('deactive')
+    $('#search').hide()
+    $('#login').hide()
+  })
+  $('.close').click(function () {
+    $('.nav-link').removeClass('hide-item')
+    $('.search-form').removeClass('active')
+    $('.close').removeClass('active')
+    $('#login').removeClass('deactive')
+    $('#search').show()
+    $('#login').show()
+  })
+
+})
+
